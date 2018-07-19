b0cc20b42d4741d410b1efc8426a97b87bdff700
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-07-18 20:18:01 -0400

    Imports

--- data_profile_viewer/database/viewsets.py
+++ data_profile_viewer/database/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from database.models import Table, Column, ValueDistribution
 from database.serializers import (
