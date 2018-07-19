28f8fae227726cfb3d9b9dc497a951e9e2997388
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-07-18 20:02:58 -0400

    Added ValueDistribution to admin

--- data_profile_viewer/database/admin.py
+++ data_profile_viewer/database/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from database.models import Table, Column, ColumnStatistics
+from database.models import Table, Column, ColumnStatistics, ValueDistribution
 
 admin.site.register(Table)
 admin.site.register(Column)
+admin.site.register(ValueDistribution)
 
 admin.site.register(ColumnStatistics)
