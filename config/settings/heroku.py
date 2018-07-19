ad13377b7aacfd720d31839d557755734cee7b09
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-07-18 23:25:05 -0400

    Made some changes to facilitate building React files for production

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -8,4 +8,20 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[])
 
 INSTALLED_APPS += ['gunicorn']  # noqa F405
 
+
+# Setup for Webpack
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend", "assets"),
+]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
+        }
+}
+
+
+# This should be last
 django_heroku.settings(locals())
+
