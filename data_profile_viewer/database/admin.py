37701891eb0ba9949aeaca16ddaac14387c75e30
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-07-19 22:28:04 -0400

    Removed the old admin

--- data_profile_viewer/database/admin.py
+++ data_profile_viewer/database/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from database.models import Table, Column, ColumnStatistics, ValueDistribution
 
 admin.site.register(Table)
-admin.site.register(Column)
 admin.site.register(ValueDistribution)
 
 admin.site.register(ColumnStatistics)
